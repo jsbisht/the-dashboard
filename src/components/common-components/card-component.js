513ffd6ebd6571955b0e0b37ddc1aef9038cb576
Jagdeep Singh Bisht <jagdeepbisht89@gmail.com>
2018-07-21 15:54:16 +0530

    Created card component

---
+++ src/components/common-components/card-component.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Card extends React.Component {
+    render() {
+        return (
+          <div className="card"></div>
+        );
+    }
+}
+
+export default Card;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,24 @@ import { render } from 'react-dom';
 
 import './styles/css/styles.css';
 
+import Card from './components/common-components/card-component';
+
 render(
-  <h1>Hello World</h1>,
+  <div className="application">
+    <header>
+      <h1 className="site-name">The Dashboard</h1>
+      <h3 className="greetings">
+        <span className="greet-user">Welcome Jagdeep.</span>
+        <span className="daily-motivation">Today is gonna be awesome.</span>
+      </h3>
+    </header>
+    <section className="dashboard">
+      <h2 className="cards-title">Your Cards</h2>
+      <Card />
+    </section>
+    <footer className="footer">
+      <h3 className="copyright">&copy; 2018</h3>
+    </footer>
+  </div>,
   document.getElementById('app')
 );
