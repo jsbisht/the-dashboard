2144a0ff9d7210cd5b49a184c53e2d5378fd3020
Jagdeep Singh Bisht <jagdeepbisht89@gmail.com>
2018-09-17 22:30:36 +0530

    Updated babel packages

--- src/components/module-components/bubbles-component/Bubble.js
+++ src/components/module-components/bubbles-component/Bubble.js
@@ -1,23 +1,46 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+//import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import "./Bubble.scss";
+
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheck, faArrowDown } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faCheck);
+library.add(faArrowDown);
 
 class Bubble extends React.Component {
   constructor(props, context) {
     super(props, context);
+
+    this.state = {
+      visible: false
+    };
   }
+
+  toggleBubbleOptions = () => {
+    this.setState({ visible: !this.state.visible });
+  };
 
   render() {
     return (
       <div className="bubble-container">
-        <div className="bubbles-options">
-          <div className="done">
+        <div
+          className={"bubble-options" + (this.state.visible ? " visible" : "")}
+        >
+          <div className="option done">
             <FontAwesomeIcon icon="check" />
           </div>
-          <div className="drop" />
+          <div className="option drop">
+            <FontAwesomeIcon icon="arrow-down" />
+          </div>
         </div>
-        <div className="bubble" />
+        <div
+          className="bubble"
+          onMouseEnter={this.toggleBubbleOptions}
+          onMouseLeave={this.toggleBubbleOptions}
+        />
       </div>
     );
   }
